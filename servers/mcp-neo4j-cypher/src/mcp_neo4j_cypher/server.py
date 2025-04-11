d4b94e294a22f99ce74314b7276ab0b1409ae6d7
alex <a.gilmore1993@gmail.com>
2025-04-11 10:02:58 -0500

    add env config for driver

--- servers/mcp-neo4j-cypher/src/mcp_neo4j_cypher/server.py
+++ servers/mcp-neo4j-cypher/src/mcp_neo4j_cypher/server.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 import mcp.types as types
 from mcp.server.fastmcp import FastMCP
@@ -11,14 +10,20 @@ import time
 import re
 import os
 from typing import Optional
-import argparse
 
 logger = logging.getLogger("mcp_neo4j_cypher")
 logger.info("Starting MCP neo4j Server")
 
-
+neo4j_driver = AsyncGraphDatabase.driver(
+    os.getenv("NEO4J_URI", "bolt://localhost:7687"),
+    auth=(
+        os.getenv("NEO4J_USERNAME", "neo4j"),
+        os.getenv("NEO4J_PASSWORD", "password"),
+    ),
+)
 
 mcp: FastMCP = FastMCP("mcp-neo4j-cypher", dependencies=["neo4j", "pydantic"])
+
 
 def _is_write_query(query: str) -> bool:
     """Check if the query is a write query."""
@@ -31,8 +36,6 @@ def _is_write_query(query: str) -> bool:
 @mcp.tool()
 async def get_neo4j_schema() -> list[types.TextContent]:
     """List all node, their attributes and their relationships to other nodes in the neo4j database"""
-
-    global neo4j_driver
 
     try:
         results = await neo4j_driver.execute_query(
@@ -62,18 +65,19 @@ async def read_neo4j_cypher(
 ) -> list[types.TextContent]:
     """Execute a read Cypher query on the neo4j database."""
 
-    global neo4j_driver
-
     if _is_write_query(query):
         raise ValueError("Only MATCH queries are allowed for read-query")
 
     try:
+
         async def read(tx: AsyncTransaction, query: str, params: dict[str, Any]):
             raw_results = await tx.run(query, params)
             results = [r.data() async for r in raw_results]
             return results
 
-        async with neo4j_driver.session(database=os.getenv("NEO4J_DATABASE", "neo4j")) as session:
+        async with neo4j_driver.session(
+            database=os.getenv("NEO4J_DATABASE", "neo4j")
+        ) as session:
             results = await session.execute_read(read, query, params)
 
             logger.debug(f"Read query returned {len(results)} rows")
@@ -84,6 +88,7 @@ async def read_neo4j_cypher(
         logger.error(f"Database error executing query: {e}\n{query}\n{params}")
         return [types.TextContent(type="text", text=f"Error: {e}\n{query}\n{params}")]
 
+
 @mcp.tool()
 async def write_neo4j_cypher(
     query: str = Field(..., description="The Cypher query to execute."),
@@ -93,16 +98,17 @@ async def write_neo4j_cypher(
 ) -> list[types.TextContent]:
     """Execute a write Cypher query on the neo4j database."""
 
-    global neo4j_driver
-
     if not _is_write_query(query):
         raise ValueError("Only write queries are allowed for write-query")
 
     try:
+
         async def write(tx: AsyncTransaction, query: str, params: dict[str, Any]):
             return await tx.run(query, params)
 
-        async with neo4j_driver.session(database=os.getenv("NEO4J_DATABASE", "neo4j")) as session:
+        async with neo4j_driver.session(
+            database=os.getenv("NEO4J_DATABASE", "neo4j")
+        ) as session:
             raw_results = await session.execute_write(write, query, params)
             counters = raw_results._summary.counters
 
@@ -137,41 +143,13 @@ def healthcheck(neo4j_driver: AsyncDriver) -> None:
         raise DatabaseError()
 
 
+def main() -> None:
 
-async def main() -> None:
-
-    global neo4j_driver
-
-    parser = argparse.ArgumentParser(description="Neo4j Cypher MCP Server")
-    parser.add_argument(
-        "--db-url", default="bolt://localhost:7687", help="Neo4j connection URL"
-    )
-    parser.add_argument("--username", default="neo4j", help="Neo4j username")
-    parser.add_argument("--password", default="password", help="Neo4j password")
-    parser.add_argument("--database", default="neo4j", help="Neo4j database")
-
-    args = parser.parse_args()
-
-    os.environ["NEO4J_URI"] = args.db_url or "localhost:7687"
-    os.environ["NEO4J_USERNAME"] = args.username or "neo4j"
-    os.environ["NEO4J_PASSWORD"] = args.password or "password"
-    os.environ["NEO4J_DATABASE"] = args.database or "neo4j"
-    
-
-    neo4j_driver = AsyncGraphDatabase.driver(
-        os.getenv("NEO4J_URI", "localhost:7687"),
-        auth=(
-            os.getenv("NEO4J_USERNAME", "neo4j"),
-            os.getenv("NEO4J_PASSWORD", "wrongpassword"),
-        ),
-    )
     # Test the connection
     healthcheck(neo4j_driver)
 
-
-    await mcp.run(transport="stdio")
-
+    mcp.run(transport="stdio")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
